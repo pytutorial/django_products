47ee9fc4607b0ce1a069c9e426654b86eb3d9be2
Tung Duong <duongthanhtungvn01@gmail.com>
2020-07-02 14:26:10 +0100

    Update

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-
+from .views_user import *
 urlpatterns = [
     path('', index, name='home'),
     path('signup', signup, name='signup'),
--- app/views.py
+++ app/views.py
@@ -6,8 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-def index(request):
-    return render(request, 'index.html')
+
 def signup(request):
     form = UserCreationForm()
     if request.method == 'POST':
